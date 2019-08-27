f83b13b8bb1c3eb608941d8d275c2775a76ed0c7
KirylPod <psossks@gmail.com>
2019-08-27 20:38:29 +0300

    Continuing practice

--- src/main/java/kye/seven/VowelCount.java
+++ src/main/java/kye/seven/VowelCount.java
@@ -1,4 +1,31 @@
 package main.java.kye.seven;
 
 public class VowelCount {
+
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+        String[] letters = str.split("");
+
+        for (int i = 0; i < letters.length; i++) {
+
+            if (letters[i].equals("a")) {
+                vowelsCount++;
+            }
+            if (letters[i].equals("e")) {
+                vowelsCount++;
+            }
+            if (letters[i].equals("i")) {
+                vowelsCount++;
+            }
+            if (letters[i].equals("o")) {
+                vowelsCount++;
+            }
+            if (letters[i].equals("u")) {
+                vowelsCount++;
+            }
+        }
+
+
+        return vowelsCount;
+    }
 }
